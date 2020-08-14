65ceac0cf795de03a8cb9c8c97c49b22e5c626d5
Shivjeet Bhullar <bhullarshivjeet@gmail.com>
2020-08-14 17:35:29 +0530

    On Work

--- Web/admin/functions.py
+++ Web/admin/functions.py
@@ -1,17 +1,48 @@
 from threading import Timer
-from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime,date,timedelta
+import atexit,random,json
 from Web import db
 
+scheduler = BackgroundScheduler()
+
 def attendance():
-    x=datetime.today()
-    secs=(x.replace(day=x.day, hour=9, minute=0, second=0, microsecond=0)-datetime.today()).seconds
-    if secs.days == -1:secs=(x.replace(day=x.day+1, hour=9, minute=0, second=0, microsecond=0)-datetime.today()).seconds
-    task = Timer(secs, do_auto_attendance)
-    task.start()
+    scheduler.add_job(func=do_auto_attendance, trigger="interval", hours=24)
+    scheduler.start()
 
 def do_auto_attendance():
-    unids = db.readkey('students',key=['unid','end'])['data']
-    for x in unids:
-     if not 'end' in x:
-      db.update("students",{'attendance':{datetime.now().strftime("%Y-%m-%d"):"Present"}},where={'unid':x['unid']})
-    attendance()
+    if date.today().weekday() != 6:
+     unids = db.readkey('students',key=['unid','end'])['data']
+     for x in unids:
+      if not 'end' in x:
+       db.update("students",{'attendance':{date.today().strftime('%Y-%m-%d'):"Present"}},where={'unid':x['unid']})
+
+def do_process(data):
+    date = datetime.strptime(data['jdate'], '%Y-%m-%d').date()
+    data['result'],data['attendance'] = {data['exam']:[]},{}
+    exam = db.read('exams',data['exam'])['data'][0]['data']
+    marks = int(len(exam)-int(data['marks'])/100*len(exam))
+    for x in exam:
+      i = random.randint(0,3)
+      x['choices']['wrong'].insert(i,x['choices']['correct'])
+      data['result'][data['exam']].append({"que":x['que'],"choices":x['choices']['wrong'],"user_choice_index":i,"correct_choice_index":i})
+    
+    while marks != 0:
+        i = random.randint(0,len(data['result'][data['exam']])-1)
+        popdata = data['result'][data['exam']].pop(i)
+        if popdata['user_choice_index']!=0:popdata['user_choice_index'] -= 1
+        else:popdata['user_choice_index'] += 1
+        data['result'][data['exam']].insert(i,popdata)
+        marks -= 1
+
+    data['result'][data['exam']] = json.dumps(data['result'][data['exam']])
+    print(datetime.strptime(data['edate'], '%Y-%m-%d').date() , date)
+    while datetime.strptime(data['edate'], '%Y-%m-%d').date() > date:
+     if date.weekday() != 6:
+       data['attendance'][date.strftime('%Y-%m-%d')] = "Present"
+       date += timedelta(days=1)
+     else:date += timedelta(days=1)
+    return data
+
+atexit.register(lambda: scheduler.shutdown())
+#APScheduler
--- Web/admin/route.py
+++ Web/admin/route.py
@@ -1,35 +1,87 @@
-from flask import Blueprint,render_template,request,redirect
+from flask import Blueprint,render_template,request,redirect,session,flash,url_for
 from quotes import Quotes
 from threading import Timer
 from datetime import datetime
 from Web import db
-from .functions import attendance
+from functools import wraps
+from .functions import attendance,do_process
 qt = Quotes()
 mod = Blueprint('admin',__name__,template_folder='templates',static_folder='./static',static_url_path='/static/cdn/')
 
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:return f(*args, **kwargs)
+        else:
+            flash("You need to login first")
+            return redirect(url_for('admin.do_admin_login'))
+    return wrap
+
+@mod.route('/login/', methods=['POST',"GET"])
+def do_admin_login():
+ if request.method == "POST":
+  if request.form['password'] == 'shivjeet' and request.form['username'] == 'Shivjeet':
+    session['logged_in'] = True
+    return redirect("/admin/")
+  else:
+    flash("Check Password Or Username!")
+    return redirect(url_for('admin.do_admin_login'))
+ else:
+   if 'logged_in' in session:return redirect("/admin/")
+   else:return render_template("admin_login.html")
+
+@mod.route("/logout")
+@login_required
+def logout():
+ session['logged_in'] = False
+ session.clear()
+ return do_admin_login()
+
+@mod.route("/read/<col>")
+@login_required
+def logoutfd(col):
+ return str(db.read(col))
+
 @mod.route('/',methods = ['GET'])
+@login_required
 def admin_index():
     return render_template('admin_index.html',qut=qt.random(),students=len(db.read('students')['data']))
 
 @mod.route('/insert',methods = ['GET','POST'])
+@login_required
 def admin_insert():
     if request.method == "POST":
-        db.write('students',request.form)
+        data = request.form.to_dict()
+        db.write('students',data)
         return render_template('admin_insert.html',qut=qt.random(),stdwrit='True')
     else:return render_template('admin_insert.html',qut=qt.random())
 
+@mod.route('/insertold',methods = ['GET','POST'])
+@login_required
+def admin_insertold():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        db.write('students',do_process(data))
+        return render_template('admin_insertold.html',qut=qt.random(),stdwrit='True',exams=db.status()['exams'])
+    else:return render_template('admin_insertold.html',qut=qt.random(),exams=db.status()['exams'])
+
 @mod.route('/delstudent/<unid>',methods = ['GET'])
+@login_required
 def admin_delstudent(unid):
     db.trash('students',where={"unid":unid})
     return render_template('admin_results.html',qut=qt.random(),students = db.read("students"))
 
 @mod.route('/result',methods = ['GET','POST'])
+@login_required
 def admin_result():
-    if request.method == "POST":return db.filter("students",request.form)['data'][0]
-    return render_template('admin_results.html',qut=qt.random(),students = db.read("students"))
+    if request.method == "POST":
+        print(db.filter("students",request.form)['data'][0])
+        return db.filter("students",request.form)['data'][0]
+    else:return render_template('admin_results.html',qut=qt.random(),students = db.read("students"))
 
 @mod.route('/exam/<req>',methods = ['GET'])
 @mod.route('/exam',methods = ['GET','POST'])
+@login_required
 def admin_exam(req=None):
   if req is not None:
       return db.read('exams',request.args['name'])['data'][0]
@@ -40,6 +92,7 @@ def admin_exam(req=None):
     return render_template('admin_exam.html',qut=qt.random(),exams=db.status())
 
 @mod.route('/question/<query>',methods = ['GET','POST'])
+@login_required
 def admin_questions(query):
    if request.method == 'POST':
      db.trash("exams",request.form['exam'],{"data":[{'$keys':['que','choices'],'$where':{'que':request.form['que'].strip()}}]})
@@ -57,8 +110,8 @@ def admin_questions(query):
     
 
 @mod.route('/attendance',methods = ['GET','POST'])
+@login_required
 def admin_attendance():
-    print(db.readkey('students',key=['unid','end'])['data'])
     if request.method == 'POST':
         if request.form['status'] == 'end':
             db.update("students",{'end':request.form['date']},where={'unid':request.form['unid']})
@@ -68,5 +121,4 @@ def admin_attendance():
             return "Absent Marked!"
     elif request.method == 'GET':return render_template('admin_attendance.html',qut=qt.random(),students = db.read("students"))
 
-
-#attendance()
+attendance()
